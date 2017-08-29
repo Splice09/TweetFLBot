7e00a90943a99bce5e4dd4b1a083e831adc4914c
Splice09 <jpf0005@gmail.com>
2017-08-28 22:17:40 -0400

    Completed recent tweet ID fetch code and updated TweetID.txt with accurate data as of Mon 8/28

--- GetRecentTweetID.py
+++ GetRecentTweetID.py
@@ -20,26 +20,31 @@ O_AUTH = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 # Initiate the connection to Twitter REST API
 TWITTER = Twitter(auth=O_AUTH)
 
-# Returns a collection of the most recent Tweets posted by the user
-# indicated by the screen_name or user_id parameters excluding replies.
-ITERATOR = TWITTER.statuses.user_timeline(screen_name="JayTrain23", exclude_replies="true")
+#Create list of players to scrape tweets from
+player_list = [line.rstrip('\n') for line in open("DauphinsTwitter.txt")]
 
 #Open file we will write tweet ID to
 MY_FILE = open("TweetID.txt", "w+")
 
-# Print the first tweet in the returned list to the screen
-TWEET_COUNT = 1
-for tweet in ITERATOR:
-    TWEET_COUNT -= 1
-    # Twitter Python Tool wraps the data returned by Twitter
-    # as a TwitterDictResponse object.
-    # We convert it back to the JSON format to print/score
-    MY_FILE.write(tweet['user']['screen_name'] +","+ json.dumps(tweet['id']) + "\r\n")
-    print "Tweet ID fetch complete."
-    print ''
+for player in player_list:
+    # Returns a collection of the most recent Tweets posted by the user
+    # indicated by the screen_name or user_id parameters excluding replies.
+    ITERATOR = TWITTER.statuses.user_timeline(screen_name= player, exclude_replies="true")
 
-    if TWEET_COUNT <= 0:
-        break
+    # Print the first tweet in the returned list to the screen
+    TWEET_COUNT = 1
+    for tweet in ITERATOR:
+        TWEET_COUNT -= 1
+        # Twitter Python Tool wraps the data returned by Twitter
+        # as a TwitterDictResponse object.
+        # We convert it back to the JSON format to print/score
+        MY_FILE.write(tweet['user']['screen_name'] +","+ json.dumps(tweet['id']) + "\r\n")
+        print "Tweet ID for " + player + " captured."
+        print ''
+
+        if TWEET_COUNT <= 0:
+            break
 
 
 MY_FILE.close()
+print "Tweet ID caputre complete."
--- test2.py
+++ test2.py
@@ -1,1 +1,5 @@
-print("This line will be printed.")
+#Create list of players to scrape tweets from
+player_list = [line.rstrip('\n') for line in open("DauphinsTwitter.txt")]
+
+for player in player_list:
+    print player
